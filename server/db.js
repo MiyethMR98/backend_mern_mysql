e6f0fcedfb694179d9f3f163d200b5e6239243f6
MIYETHMR <98822275+MIYETHMR@users.noreply.github.com>
2025-11-23 21:36:18 -0500

    Actualización backend

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 import { createPool } from 'mysql2/promise'
 
 export const pool = createPool({
-    host: 'localhost',
+    host: '192.168.1.11',
     port: 3306,
-    user: 'root',
-    password: 'faztpassword',
+    user: 'usr_usuario',
+    password: 'Ucc2025*',
     database: 'tasksdb'
 })
 
